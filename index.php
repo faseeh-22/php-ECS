6ba9808fe1e6512dda07312dba7dcaff627ba0dd
faseeh-22 <130156388+faseeh-22@users.noreply.github.com>
2024-07-05 15:08:59 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,13 @@
-<form action="action_page.php" method="post">
-  <div class="imgcontainer">
-    <img src="img_avatar2.png" alt="Avatar" class="avatar">
-  </div>
-  <div class="container">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-    <button type="submit">Login</button>
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-  </div>
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
-  </div>
-</form>
+<!-- index.php -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dummy PHP Application</title>
+</head>
+<body>
+    <h1>Hello, Docker PHP Application!</h1>
+    <p>This is a dummy PHP application running inside a Docker container.</p>
+    <p>PHP Version: <?php echo phpversion(); ?></p>
+</body>
+</html>
